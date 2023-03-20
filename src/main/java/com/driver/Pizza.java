cfd55ec3cb837196cf7291ed88b7ee32107a1ec6
abhishekpanditt <badi.abhi@gmail.com>
2023-03-20 15:05:58 +0530

    Assignment updated1

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,30 +5,71 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private int vegToppings=0;
+    private int nonVegToppings=0;
+    private int paperbag=0;
+    private int extraCheese=0;
+    int total=0;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+
+        if(isVeg) {
+            price=300;
+        }
+        else {
+            price=400;
+        }
+        total+=price;
     }
 
     public int getPrice(){
-        return this.price;
+        return total;
     }
 
-    public void addExtraCheese(){
-        // your code goes here
+    public void addExtraCheese()
+    {
+        this.extraCheese=80;
+        total+=extraCheese;
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(isVeg) {
+            this.vegToppings=70;
+            total+=vegToppings;
+        }
+        else {
+            this.nonVegToppings=120;
+            total+=nonVegToppings;
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        paperbag=20;
+        total+=paperbag;
     }
 
-    public String getBill(){
-        // your code goes here
-        return this.bill;
+    public String getBill() {
+        String ans="";
+        ans=ans+"Base Price Of The Pizza: "+price+"\n";
+
+        if(this.extraCheese!=0) {
+            ans=ans+"Extra Cheese Added: "+extraCheese+"\n";
+        }
+        if(this.vegToppings!=0) {
+            ans=ans+"Extra Toppings Added: "+vegToppings+"\n";
+        }
+        else if(this.nonVegToppings!=0) {
+            ans=ans+"Extra Toppings Added: "+nonVegToppings+"\n";
+        }
+
+        if(this.paperbag!=0) {
+            ans=ans+"Paperbag Added: "+paperbag+"\n";
+        }
+
+        total=price+paperbag+extraCheese+vegToppings+nonVegToppings;
+        ans+="Total Price: "+total+"\n";
+        this.bill=ans;
+        return ans;
     }
 }
